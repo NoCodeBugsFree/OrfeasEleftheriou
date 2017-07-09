92858677ec4d7975e4873db74b87f3d11b85e798
i5 <i5@I5-PC>
2017-07-09 16:36:49 +0300

    Initial commit

---
+++ Source/OrfeasEleftheriou/OrfeasEleftheriouGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OrfeasEleftheriouGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ORFEASELEFTHERIOU_API AOrfeasEleftheriouGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
