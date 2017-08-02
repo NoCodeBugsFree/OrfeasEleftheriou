0e16dab7b01052ccd4c9340c9bd98a4c7d1204f8
i5 <i5@I5-PC>
2017-08-02 18:02:07 +0300

    54 Creating Custom Gameplay Debugger Categories [TODO]

---
+++ Source/OrfeasEleftheriou/CustomGameplayDebugger.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "CustomGameplayDebugger.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ORFEASELEFTHERIOU_API UCustomGameplayDebugger : public UObject
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
